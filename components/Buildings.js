02f964df4c10c39ee208ea9d014a3cd934f80967
Hussain Witwit <>
2020-01-17 13:39:27 -0500

    loyoal buildings

--- components/Buildings.js
+++ components/Buildings.js
@@ -59,6 +59,52 @@ const Buildings = () => {
                 ]}
                 fillColor="rgba(76, 79, 98, 0.7)"
             />
+
+            <Polygon
+                coordinates={[
+                    { latitude: 45.457464, longitude: -73.642074 },
+                    { latitude: 45.458325, longitude: -73.641411 },
+                    { latitude: 45.458165, longitude: -73.640985 },
+                    { latitude: 45.457528, longitude: -73.641481 },
+                    { latitude: 45.457198, longitude: -73.640661 },
+                    { latitude: 45.457002, longitude: -73.640830 },
+                    { latitude: 45.457464, longitude: -73.642074 }
+                ]}
+                fillColor="rgba(76, 79, 98, 0.7)"
+            />
+
+            <Polygon
+                coordinates={[
+                    { latitude: 45.457335, longitude: -73.640715 },
+                    { latitude: 45.457179, longitude: -73.640400 },
+                    { latitude: 45.457617, longitude: -73.640051 },
+                    { latitude: 45.457826, longitude: -73.640483 },
+                    { latitude: 45.457335, longitude: -73.640715 }
+                ]}
+                fillColor="rgba(76, 79, 98, 0.7)"
+            />
+
+            <Polygon
+                coordinates={[
+                    { latitude: 45.458521, longitude: -73.640679 },
+                    { latitude: 45.458377, longitude: -73.640794 },
+                    { latitude: 45.458076, longitude: -73.639997 },
+                    { latitude: 45.458221, longitude: -73.639898 },
+                    { latitude: 45.458521, longitude: -73.640679}
+                ]}
+                fillColor="rgba(76, 79, 98, 0.7)"
+            />
+
+            <Polygon
+                coordinates={[
+                    { latitude: 45.457901, longitude: -73.640113 },
+                    { latitude: 45.458378, longitude: -73.639742 },
+                    { latitude: 45.458243, longitude: -73.639460 },
+                    { latitude: 45.457807, longitude: -73.639814 },
+                    { latitude: 45.457901, longitude: -73.640113 }
+                ]}
+                fillColor="rgba(76, 79, 98, 0.7)"
+            />
             
         </View>
     )
