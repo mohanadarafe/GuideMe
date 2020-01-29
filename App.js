27b4ccb28f85d81c3308f28c39f9315638a19a37
Kenza <kenza.boulisfane@gmail.com>
2020-01-29 15:21:38 -0500

    code cleanup

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navigator from './routes/routeStack';
 
-export default function App() {
+export default function App () {
   return (
-    <Navigator/>
+    <Navigator />
   );
 }
--- screens/Map.js
+++ screens/Map.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { Buildings } from '../components/Buildings';
 import { ToggleCampus } from '../components/ToggleCampus';
 
@@ -12,54 +12,52 @@ const mapPosition = {
         longitudeDelta: 0.01,
     },
     loyCoord: {
-        latitude: 45.457841, 
+        latitude: 45.457841,
         longitude: -73.640307,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
     }
 }
 
-export default class Map extends React.Component{
-    state = {switchValue:false}
+export default class Map extends React.Component {
+    state = { switchValue: false }
     toggleSwitch = (val) => {
         this.setState({
             switchValue: val
         })
     }
 
-    render(){
+    render () {
         return (
-            <MapView 
-                style= {styles.map} 
+            <MapView
+                style={styles.map}
                 provider={PROVIDER_GOOGLE}
                 region={this.state.switchValue ? mapPosition.sgwCoord : mapPosition.loyCoord}
                 showsUserLocation={true}
             >
-                <ToggleCampus val={this.state.switchValue} onChange={this.toggleSwitch}/>   
-                <Buildings/>
+                <ToggleCampus val={this.state.switchValue} onChange={this.toggleSwitch} />
+                <Buildings />
             </MapView>
         );
     }
-
-    
 }
 
 export const styles = StyleSheet.create({
     map: {
-      height: '100%',
-      flex: 1,
-      //Main axis
-      flexDirection:"row",
-      //Describes how to align children along the cross axis of their container
-      alignItems: "flex-end",
-      //Describes how to align children within the main axis of their container
-      justifyContent: "flex-end",
+        height: '100%',
+        flex: 1,
+        //Main axis
+        flexDirection: "row",
+        //Describes how to align children along the cross axis of their container
+        alignItems: "flex-end",
+        //Describes how to align children within the main axis of their container
+        justifyContent: "flex-end",
     },
     buildingIdentification: {
         color: "blue",
-        opacity: 0.5,	
-       fontSize: 10,
-       fontWeight: "bold"
+        opacity: 0.5,
+        fontSize: 10,
+        fontWeight: "bold"
     },
     switch: {
         height: 70,
