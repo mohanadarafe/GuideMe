dbc732d74eacfb06b90164167772babf04acf112
Badreddine Loulidi <badred123456@gmail.com>
2020-02-22 14:25:46 -0500

    fixed merge issues

--- components/ToggleCampus.js
+++ components/ToggleCampus.js
@@ -2,25 +2,6 @@ import React from 'react';
 import { Switch } from 'react-native';
 import { styles } from '../screens/Map'
 
-<<<<<<< HEAD
-/**
- * Toggle Switch between Campus
- * Prop1: val | value of the switch
- * Prop2: onChange | change value of switch
- * <ToggleCampus val={this.state.switchValue} onChange={this.toggleSwitch}/>
- */
-export default class ToggleCampus extends React.Component {
-    render () {
-        return (
-            <Switch
-                style={styles.switch}
-                value={this.props.val}
-                onValueChange = {(val) => this.props.onChange(val)}
-                trackColor={{true: "green", false: "red"}}
-            ></Switch>
-        );
-    }
-=======
 
 function ToggleCampus (props) {
     return (
@@ -31,7 +12,6 @@ function ToggleCampus (props) {
             trackColor={{ true: "green", false: "red" }}
         ></Switch>
     );
->>>>>>> a2f585f4e900a93b5be5584f849f0b0adc578e55
 }
 
 export { ToggleCampus };
--- screens/Map.js
+++ screens/Map.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
-import { StyleSheet, View,  ActionSheetIOS } from 'react-native';
-import MapView, {PROVIDER_GOOGLE, Callout} from 'react-native-maps';
-import { Buildings } from '../components/Buildings';
-=======
 import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { BuildingHighlight } from '../components/BuildingHighlight';
->>>>>>> a2f585f4e900a93b5be5584f849f0b0adc578e55
 import { ToggleCampus } from '../components/ToggleCampus';
 import { BuildingIdentification } from '../components/BuildingIdentification';
 import { BottomMenu } from '../components/BottomMenu';
@@ -28,69 +21,6 @@ const mapPosition = {
     }
 }
 
-<<<<<<< HEAD
-export default class Map extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            latitude: null,
-            longitude: null
-        };       
-    }
-    state = { switchValue: false }
-    toggleSwitch = (val) => {
-        this.setState({
-            switchValue: val
-        })
-    }
-
-	findCoordinates = () => {
-        let polygon = [
-            { latitude: 45.545140, longitude: -73.566827 },
-            { latitude: 45.544366, longitude: -73.563919 },
-            { latitude: 45.546057, longitude: -73.562503 },
-            { latitude: 45.550340, longitude: -73.565317 },
-            { latitude: 45.545140, longitude: -73.566827 }
-          ];
-    
-        //Watch position is working
-		navigator.geolocation.watchPosition(
-            (position) => {
-                let point = {
-                  lat: position.coords.latitude,
-                  lng: position.coords.longitude
-                };
-                console.log(position)
-                
-                //TODO Find a way to compare the current location "position" with the polygon inside of buildings.js
-                //Point is within polygon but it returns NOT within
-                GeoFencing.containsLocation(point, polygon)
-                  .then(() => console.log('point is within polygon'))
-                  .catch(() => console.log('point is NOT within polygon'))
-              },
-              (error) => alert(error.message),
-              { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-            );
-          }
-
-    render(){
-        this.findCoordinates();
-        return (
-            <MapView 
-                style={styles.map} 
-                provider={PROVIDER_GOOGLE}
-                region={this.state.switchValue ? mapPosition.sgwCoord : mapPosition.loyCoord}
-                showsUserLocation={true}
-                minZoomLevel={16}
-            >
-            <ToggleCampus val={this.state.switchValue} onChange={this.toggleSwitch}/>   
-            <Buildings/>
-            <BuildingIdentification/>
-            </MapView>
-        );
-    }
-=======
 function Map () {
     const [switchVal, setswitchVal] = React.useState(true);
 
@@ -108,27 +38,10 @@ function Map () {
             <ToggleCampus val={switchVal} onChange={setswitchVal} />
         </MapView>
     );
->>>>>>> a2f585f4e900a93b5be5584f849f0b0adc578e55
 }
 
 export const styles = StyleSheet.create({
     map: {
-<<<<<<< HEAD
-      height: '100%',
-      flex: 1,
-      //Main axis
-      flexDirection:"row",
-      //Describes how to align children along the cross axis of their container
-      alignItems: "flex-end",
-      //Describes how to align children within the main axis of their container
-      justifyContent: "flex-end",
-    },
-    buildingIdentification: {
-        color: "blue",
-        opacity: 0.5,	
-       fontSize: 10,
-       fontWeight: "bold",
-=======
         height: '100%',
         flex: 1,
         //Main axis
@@ -143,16 +56,11 @@ export const styles = StyleSheet.create({
         textShadowColor: '#000000',
         textShadowOffset: { width: 1, height: 4 },
         textShadowRadius: 5
->>>>>>> a2f585f4e900a93b5be5584f849f0b0adc578e55
     },
     switch: {
         height: 140,
         width: 70
     },
-<<<<<<< HEAD
-});
-=======
 });
 
 export default Map;
->>>>>>> a2f585f4e900a93b5be5584f849f0b0adc578e55
