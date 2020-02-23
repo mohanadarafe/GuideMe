d237638419d89765975bf2da5bf1409dae5ef779
Alain D. Jobs <33582344+AlainJobs@users.noreply.github.com>
2020-02-22 22:11:58 -0500

    Put textView back to absolute positionning.

--- components/BottomMenu.js
+++ components/BottomMenu.js
@@ -36,7 +36,7 @@ function BottomMenu () {
             <AppLoading />
         )
     }
-    
+  
 }
 
 export const styles = StyleSheet.create({
@@ -46,29 +46,30 @@ export const styles = StyleSheet.create({
         position: 'absolute',
         borderRadius: 30.5,
         backgroundColor: '#2A2E43',
-        flexDirection: 'row',
-        justifyContent: 'center',
+        // flexDirection: 'row',
+        // flex: 1,
+        // alignContent: 'stretch',
+        // justifyContent: 'center',
+        // flexWrap: 'wrap',
         bottom: -275
     },
     toggle: {
-        left: "30%",
+        position: 'absolute',
+        left: "80%",
         top: "6.5%"
     },
     mainLabel: {
         position: 'absolute', 
         top: '5%', 
-        left: '15%', 
-        right: 0, 
-        bottom: 0, 
-        justifyContent: 'center', 
-        alignItems: 'center',
+        left: '10%', 
         color: '#FFFFFF',
-        fontSize: 18,
+        fontSize: 20,
         fontFamily: 'encodeSansExpanded'
     },
     shortLabel: {
-        top: '8%', 
-        right: '30%', 
+        position: 'absolute', 
+        top: '12%',
+        left: '10%', 
         color: '#80828D',
         fontSize: 16,
         fontFamily: 'encodeSansExpanded'
