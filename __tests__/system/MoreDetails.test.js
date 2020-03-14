da27ad77fd78625d569a27d5ac7051075feb2c81
Kenza <kenza.boulisfane@gmail.com>
2020-03-14 19:45:25 -0400

    fix: failing MoreDetails test

--- __tests__/system/MoreDetails.test.js
+++ __tests__/system/MoreDetails.test.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { MoreDetails } from '../../screens/MoreDetails';
-import {findByTestAtrr} from '../../utils/Index';
+import React from "react";
+import { shallow } from "enzyme";
+import MoreDetails from "../../screens/MoreDetails";
+import { findByTestAtrr } from "../../utils/Index";
 
-const setUp = (props={}) => {
-   const component = shallow(<MoreDetails {...props} />);
-   return component;
+const setUp = (props = {}) => {
+    const component = shallow(<MoreDetails {...props} />);
+    return component;
 };
 
-describe('MoreDetails Component', () => {
-   let component;
-   beforeEach(() => {
-       component = setUp();
-   });
-   it('Should render without errors', () => {
-       const wrapper = findByTestAtrr(component, 'MoreDetails');
-       expect(wrapper.length).toBe(0);
-   });
+describe("MoreDetails Component", () => {
+    let component;
+    beforeEach(() => {
+        component = setUp();
+    });
+    it("Should render without errors", () => {
+        const wrapper = findByTestAtrr(component, "MoreDetailsComponent");
+        expect(wrapper.length).toBe(0);
+    });
 });
