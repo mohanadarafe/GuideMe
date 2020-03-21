195cd8fcecaea7efcd9520090f14a4227ceb59d5
Kenza <kenza.boulisfane@gmail.com>
2020-03-20 20:53:50 -0400

    feat: add searchbar and typing test to the main screen (#160)

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -8,6 +8,10 @@ describe("Main screen", () => {
 
   it.skip("should have a map and a searchbar", async () => {
     await expect(element(by.id("mapview"))).toBeVisible();
+    await expect(element(by.id("search"))).toBeVisible();
+  });
+  it.skip("Should click on the searchbar and type: hall building", async () => {
     await element(by.id("search")).tap();
+    await element(by.id("search")).typeText("hall building");
   });
 });
