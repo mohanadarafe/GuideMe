b5795f8e2a095fc8127c5394780bf3cb34187cd4
Kenza <kenza.boulisfane@gmail.com>
2020-03-21 16:23:08 -0400

    feat: add test scenario in the comments

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -5,6 +5,19 @@ describe("Main screen", () => {
   beforeEach(async () => {
     await reloadApp();
   });
+
+  /**
+  * The following source code represents the following automated test scenario
+  * 1. Load the app
+  * 2. Display main screen
+  * 3. Click on the searchbar
+  * 4. Type the query: "hall building"
+  * 5. Select the corresponding item from the dropdown list
+  * 6. Click on the bottom menu (not yet implemented)
+  * 7. Scroll down and read the information provided (not yet implemented)
+  * includes a toggle (US6) & an arrow icon leading to the More Details page.
+  */
+
   it.skip("Should click on the searchbar and type: hall building", async () => {
     await element(by.id("search")).tap();
     await element(by.id("search")).typeText("hall building");
