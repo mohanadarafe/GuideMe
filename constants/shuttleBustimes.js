55387d6777f77fcc9fd39d65ed23a15d35825072
Kenza <kenza.boulisfane@gmail.com>
2020-04-02 17:12:25 -0400

    fix: add return to shuttleBusTime

--- constants/shuttleBustimes.js
+++ constants/shuttleBustimes.js
@@ -1,204 +1,204 @@
 export const ShuttleBusTimes = () => {
-    var times = [
-        schedule["Loyola"] = {
-            MondayToThursday: [
-                {
-                    hour: 7,
-                    minutes: 30
-                },
-                {
-                    hour: 7,
-                    minutes: 40
-                },
-                {
-                    hour: 7,
-                    minutes: 55
-                },
-                {
-                    hour: 8,
-                    minutes: 20
-                },
-                {
-                    hour: 8,
-                    minutes: 35
-                },
-                {
-                    hour: 8,
-                    minutes: 55
-                },
-                {
-                    hour: 9,
-                    minutes: 10
-                },
-                {
-                    hour: 9,
-                    minutes: 30
-                }, {
-                    hour: 9,
-                    minutes: 45
-                }, {
-                    hour: 10,
-                    minutes: 20
-                }, {
-                    hour: 10,
-                    minutes: 35
-                }, {
-                    hour: 10,
-                    minutes: 55
-                }, {
-                    hour: 11,
-                    minutes: 10
-                }, {
-                    hour: 11,
-                    minutes: 30
-                }, {
-                    hour: 12,
-                    minutes: 0
-                }, {
-                    hour: 12,
-                    minutes: 30
-                }, {
-                    hour: 13,
-                    minutes: 0
-                }, {
-                    hour: 13,
-                    minutes: 30
-                }, {
-                    hour: 14,
-                    minutes: 0
-                }, {
-                    hour: 14,
-                    minutes: 30
-                }, {
-                    hour: 15,
-                    minutes: 0
-                }, {
-                    hour: 15,
-                    minutes: 30
-                }, {
-                    hour: 16,
-                    minutes: 0
-                }, {
-                    hour: 16,
-                    minutes: 30
-                }, {
-                    hour: 17,
-                    minutes: 0
-                }, {
-                    hour: 17,
-                    minutes: 30
-                }, {
-                    hour: 18,
-                    minutes: 0
-                }, {
-                    hour: 18,
-                    minutes: 30
-                }, {
-                    hour: 19,
-                    minutes: 0
-                }, {
-                    hour: 19,
-                    minutes: 30
-                }, {
-                    hour: 20,
-                    minutes: 0
-                }, {
-                    hour: 20,
-                    minutes: 30
-                }, {
-                    hour: 20,
-                    minutes: 50
-                },
-                {
-                    hour: 21,
-                    minutes: 5
-                }, {
-                    hour: 21,
-                    minutes: 30
-                }, {
-                    hour: 22,
-                    minutes: 0
-                }, {
-                    hour: 22,
-                    minutes: 30
-                },
-                {
-                    hour: 23,
-                    minutes: 0
-                },
-            ],
-            Friday: [
-                {
-                    hour: 7,
-                    minutes: 40
-                },
-                {
-                    hour: 8,
-                    minutes: 15
-                }, {
-                    hour: 8,
-                    minutes: 55
-                }, {
-                    hour: 9,
-                    minutes: 30
-                }, {
-                    hour: 10,
-                    minutes: 20
-                }, {
-                    hour: 10,
-                    minutes: 35
-                }, {
-                    hour: 11,
-                    minutes: 10
-                }, {
-                    hour: 11,
-                    minutes: 45
-                }, {
-                    hour: 12,
-                    minutes: 20
-                }, {
-                    hour: 12,
-                    minutes: 55
-                }, {
-                    hour: 13,
-                    minutes: 30
-                }, {
-                    hour: 14,
-                    minutes: 5
-                }, {
-                    hour: 14,
-                    minutes: 40
-                }, {
-                    hour: 15,
-                    minutes: 15
-                }, {
-                    hour: 15,
-                    minutes: 50
-                }, {
-                    hour: 16,
-                    minutes: 25
-                }, {
-                    hour: 16,
-                    minutes: 40
-                },
-                {
-                    hour: 17,
-                    minutes: 0
-                }, {
-                    hour: 18,
-                    minutes: 5
-                }, {
-                    hour: 18,
-                    minutes: 40
-                }, {
-                    hour: 19,
-                    minutes: 15
-                }, {
-                    hour: 19,
-                    minutes: 50
-                },
-            ]
-        },
+    var schedule = [];
+    schedule["Loyola"] = {
+        MondayToThursday: [
+            {
+                hour: 7,
+                minutes: 30
+            },
+            {
+                hour: 7,
+                minutes: 40
+            },
+            {
+                hour: 7,
+                minutes: 55
+            },
+            {
+                hour: 8,
+                minutes: 20
+            },
+            {
+                hour: 8,
+                minutes: 35
+            },
+            {
+                hour: 8,
+                minutes: 55
+            },
+            {
+                hour: 9,
+                minutes: 10
+            },
+            {
+                hour: 9,
+                minutes: 30
+            }, {
+                hour: 9,
+                minutes: 45
+            }, {
+                hour: 10,
+                minutes: 20
+            }, {
+                hour: 10,
+                minutes: 35
+            }, {
+                hour: 10,
+                minutes: 55
+            }, {
+                hour: 11,
+                minutes: 10
+            }, {
+                hour: 11,
+                minutes: 30
+            }, {
+                hour: 12,
+                minutes: 0
+            }, {
+                hour: 12,
+                minutes: 30
+            }, {
+                hour: 13,
+                minutes: 0
+            }, {
+                hour: 13,
+                minutes: 30
+            }, {
+                hour: 14,
+                minutes: 0
+            }, {
+                hour: 14,
+                minutes: 30
+            }, {
+                hour: 15,
+                minutes: 0
+            }, {
+                hour: 15,
+                minutes: 30
+            }, {
+                hour: 16,
+                minutes: 0
+            }, {
+                hour: 16,
+                minutes: 30
+            }, {
+                hour: 17,
+                minutes: 0
+            }, {
+                hour: 17,
+                minutes: 30
+            }, {
+                hour: 18,
+                minutes: 0
+            }, {
+                hour: 18,
+                minutes: 30
+            }, {
+                hour: 19,
+                minutes: 0
+            }, {
+                hour: 19,
+                minutes: 30
+            }, {
+                hour: 20,
+                minutes: 0
+            }, {
+                hour: 20,
+                minutes: 30
+            }, {
+                hour: 20,
+                minutes: 50
+            },
+            {
+                hour: 21,
+                minutes: 5
+            }, {
+                hour: 21,
+                minutes: 30
+            }, {
+                hour: 22,
+                minutes: 0
+            }, {
+                hour: 22,
+                minutes: 30
+            },
+            {
+                hour: 23,
+                minutes: 0
+            },
+        ],
+        Friday: [
+            {
+                hour: 7,
+                minutes: 40
+            },
+            {
+                hour: 8,
+                minutes: 15
+            }, {
+                hour: 8,
+                minutes: 55
+            }, {
+                hour: 9,
+                minutes: 30
+            }, {
+                hour: 10,
+                minutes: 20
+            }, {
+                hour: 10,
+                minutes: 35
+            }, {
+                hour: 11,
+                minutes: 10
+            }, {
+                hour: 11,
+                minutes: 45
+            }, {
+                hour: 12,
+                minutes: 20
+            }, {
+                hour: 12,
+                minutes: 55
+            }, {
+                hour: 13,
+                minutes: 30
+            }, {
+                hour: 14,
+                minutes: 5
+            }, {
+                hour: 14,
+                minutes: 40
+            }, {
+                hour: 15,
+                minutes: 15
+            }, {
+                hour: 15,
+                minutes: 50
+            }, {
+                hour: 16,
+                minutes: 25
+            }, {
+                hour: 16,
+                minutes: 40
+            },
+            {
+                hour: 17,
+                minutes: 0
+            }, {
+                hour: 18,
+                minutes: 5
+            }, {
+                hour: 18,
+                minutes: 40
+            }, {
+                hour: 19,
+                minutes: 15
+            }, {
+                hour: 19,
+                minutes: 50
+            },
+        ]
+    },
         schedule["SGW"] = {
             MondayToThursday: [
                 {
@@ -386,6 +386,6 @@ export const ShuttleBusTimes = () => {
                     minutes: 50
                 },
             ]
-        },
-    ];
+        };
+    return schedule;
 };
