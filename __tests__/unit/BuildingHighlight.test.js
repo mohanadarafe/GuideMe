2b913a394e2c1670420acb23f70a7a9b7d359c47
Alain D. Jobs <33582344+AlainJobs@users.noreply.github.com>
2020-04-10 05:26:08 -0400

    Updated unit test for BH
    BH: BuildingHighlights

--- __tests__/unit/BuildingHighlight.test.js
+++ __tests__/unit/BuildingHighlight.test.js
@@ -2,25 +2,44 @@ import React from "react";
 import { BuildingHighlight } from "../../components/BuildingHighlight";
 import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
+import configureMockStore from "redux-mock-store";
+
+const mockStore = configureMockStore();
+
 
 describe("Building Highlight component", () => {
   let wrapper;
   const setState = jest.fn();
   const useStateSpy = jest.spyOn(React, "useState");
   useStateSpy.mockImplementation((init) => [init, setState]);
-
+  let component;
+  const props = {
+    isFetching: false,
+    dispatch: jest.fn(),
+    setSelectedBuildingName: (value) => {return value},
+  }
   beforeEach(() => {
-    wrapper = shallow(<BuildingHighlight />);
+    wrapper = shallow(<BuildingHighlight {...props}/>);
   });
 
+  // component = renderer.create(
+  //   <Provider store={store}>
+  //     <BuildingHighlight />
+  //   </Provider>
+  // );
+
   test("renders correctly", () => {
-    const tree = renderer.create(<BuildingHighlight />).toJSON();
+    const tree = renderer.create(<BuildingHighlight {...props}/>).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   test("number of polygons", () => {
-    const tree = renderer.create(<BuildingHighlight />).toJSON();
+    const tree = renderer.create(<BuildingHighlight {...props}/>).toJSON();
     expect(tree.children.length).toBe(11);
+  });
+
+  it('should dispatch an action on button click', () => {
+
   });
 
   test("pressing on a building", () => {
--- __tests__/unit/PreferenceMenu.test.js
+++ __tests__/unit/PreferenceMenu.test.js
@@ -1,12 +1,10 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { Text } from "react-native";
 import  PreferenceMenu  from "../../screens/PreferenceMenu";
 import configureMockStore from "redux-mock-store";
 import { Provider } from "react-redux";
 import { shallow } from 'enzyme';
-import { Button } from "native-base";
-import {findByTestAtrr} from "../../utils/Index";
+
 
 
 const mockStore = configureMockStore();
