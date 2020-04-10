f58475b37ae3821b85923244dc40e10b61873b4e
Alain D. Jobs <33582344+AlainJobs@users.noreply.github.com>
2020-04-09 20:35:45 -0400

    Modified unit test PrefrenceMenu
    #221

--- __tests__/unit/PreferenceMenu.test.js
+++ __tests__/unit/PreferenceMenu.test.js
@@ -1,15 +1,38 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import PreferenceMenu from "../../screens/PreferenceMenu";
+import { Text } from "react-native";
+import  PreferenceMenu  from "../../screens/PreferenceMenu";
+import configureMockStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { shallow } from 'enzyme';
+import { Button } from "native-base";
+import {findByTestAtrr} from "../../utils/Index";
 
-describe("Preference Menu component", () => {
-  test("renders correctly", () => {
-    const navigation = {
-      getParam: (param, defaultValue) => {
-        return defaultValue
-      }
-    };
-    const tree = renderer.create(<PreferenceMenu navigation={navigation} />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+
+const mockStore = configureMockStore();
+const store = mockStore({});
+
+const setUp = (props={}) => {
+  const navigation = {
+    getParam: (param, defaultValue) => {
+      return defaultValue
+    }
+  };
+    const component = shallow( 
+    <Provider store={store}>
+      <PreferenceMenu navigation ={navigation} />
+  </Provider>);
+    return component;
+  };
+
+
+describe("Preference Menu Screen", () => {
+ 
+   const PreferenceMenuWrapper =  setUp();
+
+    it("Should render without errors", () => {
+      const tree = renderer.create(PreferenceMenuWrapper).toJSON();
+      expect(tree).toMatchSnapshot();
+
+    });
 });
