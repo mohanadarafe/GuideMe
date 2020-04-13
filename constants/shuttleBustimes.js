0167cf1bb82bfe780dbf0de58abfdbf5c0d154de
Kenza <kenza.boulisfane@gmail.com>
2020-04-13 19:27:46 -0400

    Fix shuttle bus times convert hours & minutes to strings to allow double digits [#167]

--- constants/shuttleBustimes.js
+++ constants/shuttleBustimes.js
@@ -3,386 +3,386 @@ export const ShuttleBusTimes = () => {
     schedule["Loyola"] = {
         MondayToThursday: [
             {
-                hour: 7,
-                minutes: 30
+                hour: "7",
+                minutes: "30"
             },
             {
-                hour: 7,
-                minutes: 40
+                hour: "7",
+                minutes: "40"
             },
             {
-                hour: 7,
-                minutes: 55
+                hour: "7",
+                minutes: "55"
             },
             {
-                hour: 8,
-                minutes: 20
+                hour: "8",
+                minutes: "20"
             },
             {
-                hour: 8,
-                minutes: 35
+                hour: "8",
+                minutes: "35"
             },
             {
-                hour: 8,
-                minutes: 55
+                hour: "8",
+                minutes: "55"
             },
             {
-                hour: 9,
-                minutes: 10
+                hour: "9",
+                minutes: "10"
             }, {
-                hour: 9,
-                minutes: 30
+                hour: "9",
+                minutes: "30"
             }, {
-                hour: 9,
-                minutes: 45
+                hour: "9",
+                minutes: "45"
             }, {
-                hour: 10,
-                minutes: 20
+                hour: "10",
+                minutes: "20"
             }, {
-                hour: 10,
-                minutes: 35
+                hour: "10",
+                minutes: "35"
             }, {
-                hour: 10,
-                minutes: 55
+                hour: "10",
+                minutes: "55"
             }, {
-                hour: 11,
-                minutes: 10
+                hour: "11",
+                minutes: "10"
             }, {
-                hour: 11,
-                minutes: 30
+                hour: "11",
+                minutes: "30"
             }, {
-                hour: 12,
-                minutes: 0
+                hour: "12",
+                minutes: "00"
             }, {
-                hour: 12,
-                minutes: 30
+                hour: "12",
+                minutes: "30"
             }, {
-                hour: 13,
-                minutes: 0
+                hour: "13",
+                minutes: "00"
             }, {
-                hour: 13,
-                minutes: 30
+                hour: "13",
+                minutes: "30"
             }, {
-                hour: 14,
-                minutes: 0
+                hour: "14",
+                minutes: "00"
             }, {
-                hour: 14,
-                minutes: 30
+                hour: "14",
+                minutes: "30"
             }, {
-                hour: 15,
-                minutes: 0
+                hour: "15",
+                minutes: "00"
             }, {
-                hour: 15,
-                minutes: 30
+                hour: "15",
+                minutes: "30"
             }, {
-                hour: 16,
-                minutes: 0
+                hour: "16",
+                minutes: "00"
             }, {
-                hour: 16,
-                minutes: 30
+                hour: "16",
+                minutes: "30"
             }, {
-                hour: 17,
-                minutes: 0
+                hour: "17",
+                minutes: "00"
             }, {
-                hour: 17,
-                minutes: 30
+                hour: "17",
+                minutes: "30"
             }, {
-                hour: 18,
-                minutes: 0
+                hour: "18",
+                minutes: "00"
             }, {
-                hour: 18,
-                minutes: 30
+                hour: "18",
+                minutes: "30"
             }, {
-                hour: 19,
-                minutes: 0
+                hour: "19",
+                minutes: "00"
             }, {
-                hour: 19,
-                minutes: 30
+                hour: "19",
+                minutes: "30"
             }, {
-                hour: 20,
-                minutes: 0
+                hour: "20",
+                minutes: "00"
             }, {
-                hour: 20,
-                minutes: 30
+                hour: "20",
+                minutes: "30"
             }, {
-                hour: 20,
-                minutes: 50
+                hour: "20",
+                minutes: "50"
             },
             {
-                hour: 21,
-                minutes: 5
+                hour: "21",
+                minutes: "05"
             }, {
-                hour: 21,
-                minutes: 30
+                hour: "21",
+                minutes: "30"
             }, {
-                hour: 22,
-                minutes: 0
+                hour: "22",
+                minutes: "00"
             }, {
-                hour: 22,
-                minutes: 30
+                hour: "22",
+                minutes: "30"
             },
             {
-                hour: 23,
-                minutes: 0
+                hour: "23",
+                minutes: "00"
             },
         ],
         Friday: [
             {
-                hour: 7,
-                minutes: 40
+                hour: "7",
+                minutes: "40"
             },
             {
-                hour: 8,
-                minutes: 15
+                hour: "8",
+                minutes: "15"
             }, {
-                hour: 8,
-                minutes: 55
+                hour: "8",
+                minutes: "55"
             }, {
-                hour: 9,
-                minutes: 30
+                hour: "9",
+                minutes: "30"
             }, {
-                hour: 10,
-                minutes: 20
+                hour: "10",
+                minutes: "20"
             }, {
-                hour: 10,
-                minutes: 35
+                hour: "10",
+                minutes: "35"
             }, {
-                hour: 11,
-                minutes: 10
+                hour: "11",
+                minutes: "10"
             }, {
-                hour: 11,
-                minutes: 45
+                hour: "11",
+                minutes: "45"
             }, {
-                hour: 12,
-                minutes: 20
+                hour: "12",
+                minutes: "20"
             }, {
-                hour: 12,
-                minutes: 55
+                hour: "12",
+                minutes: "55"
             }, {
-                hour: 13,
-                minutes: 30
+                hour: "13",
+                minutes: "30"
             }, {
-                hour: 14,
-                minutes: 5
+                hour: "14",
+                minutes: "05"
             }, {
-                hour: 14,
-                minutes: 40
+                hour: "14",
+                minutes: "40"
             }, {
-                hour: 15,
-                minutes: 15
+                hour: "15",
+                minutes: "15"
             }, {
-                hour: 15,
-                minutes: 50
+                hour: "15",
+                minutes: "50"
             }, {
-                hour: 16,
-                minutes: 25
+                hour: "16",
+                minutes: "25"
             }, {
-                hour: 16,
-                minutes: 40
+                hour: "16",
+                minutes: "40"
             },
             {
-                hour: 17,
-                minutes: 0
+                hour: "17",
+                minutes: "00"
             }, {
-                hour: 18,
-                minutes: 5
+                hour: "18",
+                minutes: "05"
             }, {
-                hour: 18,
-                minutes: 40
+                hour: "18",
+                minutes: "40"
             }, {
-                hour: 19,
-                minutes: 15
+                hour: "19",
+                minutes: "15"
             }, {
-                hour: 19,
-                minutes: 50
+                hour: "19",
+                minutes: "50"
             },
         ]
     },
         schedule["SGW"] = {
             MondayToThursday: [
                 {
-                    hour: 7,
-                    minutes: 45
+                    hour: "7",
+                    minutes: "45"
                 }, {
-                    hour: 8,
-                    minutes: 5
+                    hour: "8",
+                    minutes: "05"
                 }, {
-                    hour: 8,
-                    minutes: 20
+                    hour: "8",
+                    minutes: "20"
                 }, {
-                    hour: 8,
-                    minutes: 35
+                    hour: "8",
+                    minutes: "35"
                 }, {
-                    hour: 8,
-                    minutes: 55
+                    hour: "8",
+                    minutes: "55"
                 }, {
-                    hour: 9,
-                    minutes: 10
+                    hour: "9",
+                    minutes: "10"
                 }, {
-                    hour: 9,
-                    minutes: 30
+                    hour: "9",
+                    minutes: "30"
                 }, {
-                    hour: 9,
-                    minutes: 45
+                    hour: "9",
+                    minutes: "45"
                 }, {
-                    hour: 10,
-                    minutes: 5
+                    hour: "10",
+                    minutes: "05"
                 }, {
-                    hour: 10,
-                    minutes: 20
+                    hour: "10",
+                    minutes: "20"
                 }, {
-                    hour: 10,
-                    minutes: 55
+                    hour: "10",
+                    minutes: "55"
                 }, {
-                    hour: 11,
-                    minutes: 10
+                    hour: "11",
+                    minutes: "10"
                 }, {
-                    hour: 11,
-                    minutes: 45
+                    hour: "11",
+                    minutes: "45"
                 }, {
-                    hour: 12,
-                    minutes: 15
+                    hour: "12",
+                    minutes: "15"
                 }, {
-                    hour: 12,
-                    minutes: 45
+                    hour: "12",
+                    minutes: "45"
                 }, {
-                    hour: 13,
-                    minutes: 15
+                    hour: "13",
+                    minutes: "15"
                 }, {
-                    hour: 13,
-                    minutes: 45
+                    hour: "13",
+                    minutes: "45"
                 }, {
-                    hour: 14,
-                    minutes: 15
+                    hour: "14",
+                    minutes: "15"
                 }, {
-                    hour: 14,
-                    minutes: 45
+                    hour: "14",
+                    minutes: "45"
                 }, {
-                    hour: 15,
-                    minutes: 15
+                    hour: "15",
+                    minutes: "15"
                 }, {
-                    hour: 15,
-                    minutes: 45
+                    hour: "15",
+                    minutes: "45"
                 }, {
-                    hour: 16,
-                    minutes: 15
+                    hour: "16",
+                    minutes: "15"
                 }, {
-                    hour: 16,
-                    minutes: 45
+                    hour: "16",
+                    minutes: "45"
                 }, {
-                    hour: 17,
-                    minutes: 15
+                    hour: "17",
+                    minutes: "15"
                 }, {
-                    hour: 17,
-                    minutes: 45
+                    hour: "17",
+                    minutes: "45"
                 }, {
-                    hour: 18,
-                    minutes: 15
+                    hour: "18",
+                    minutes: "15"
                 }, {
-                    hour: 18,
-                    minutes: 45
+                    hour: "18",
+                    minutes: "45"
                 }, {
-                    hour: 19,
-                    minutes: 15
+                    hour: "19",
+                    minutes: "15"
                 }, {
-                    hour: 19,
-                    minutes: 45
+                    hour: "19",
+                    minutes: "45"
                 }, {
-                    hour: 20,
-                    minutes: 0
+                    hour: "20",
+                    minutes: "00"
                 }, {
-                    hour: 20,
-                    minutes: 15
+                    hour: "20",
+                    minutes: "15"
                 }, {
-                    hour: 20,
-                    minutes: 30
+                    hour: "20",
+                    minutes: "30"
                 }, {
-                    hour: 21,
-                    minutes: 0
+                    hour: "21",
+                    minutes: "00"
                 }, {
-                    hour: 21,
-                    minutes: 25
+                    hour: "21",
+                    minutes: "25"
                 }, {
-                    hour: 21,
-                    minutes: 45
+                    hour: "21",
+                    minutes: "45"
                 }, {
-                    hour: 22,
-                    minutes: 5
+                    hour: "22",
+                    minutes: "05"
                 }, {
-                    hour: 22,
-                    minutes: 30
+                    hour: "22",
+                    minutes: "30"
                 }, {
-                    hour: 23,
-                    minutes: 0
+                    hour: "23",
+                    minutes: "00"
                 },
             ],
             Friday: [
                 {
-                    hour: 7,
-                    minutes: 45
+                    hour: "7",
+                    minutes: "45"
                 }, {
-                    hour: 8,
-                    minutes: 20
+                    hour: "8",
+                    minutes: "20"
                 }, {
-                    hour: 8,
-                    minutes: 55
+                    hour: "8",
+                    minutes: "55"
                 }, {
-                    hour: 9,
-                    minutes: 30
+                    hour: "9",
+                    minutes: "30"
                 }, {
-                    hour: 10,
-                    minutes: 5
+                    hour: "10",
+                    minutes: "05"
                 }, {
-                    hour: 10,
-                    minutes: 55
+                    hour: "10",
+                    minutes: "55"
                 }, {
-                    hour: 11,
-                    minutes: 10
+                    hour: "11",
+                    minutes: "10"
                 }, {
-                    hour: 11,
-                    minutes: 45
+                    hour: "11",
+                    minutes: "45"
                 }, {
-                    hour: 12,
-                    minutes: 20
+                    hour: "12",
+                    minutes: "20"
                 }, {
-                    hour: 12,
-                    minutes: 55
+                    hour: "12",
+                    minutes: "55"
                 }, {
-                    hour: 13,
-                    minutes: 30
+                    hour: "13",
+                    minutes: "30"
                 }, {
-                    hour: 14,
-                    minutes: 5
+                    hour: "14",
+                    minutes: "05"
                 }, {
-                    hour: 14,
-                    minutes: 40
+                    hour: "14",
+                    minutes: "40"
                 }, {
-                    hour: 15,
-                    minutes: 15
+                    hour: "15",
+                    minutes: "15"
                 }, {
-                    hour: 15,
-                    minutes: 50
+                    hour: "15",
+                    minutes: "50"
                 }, {
-                    hour: 16,
-                    minutes: 25
+                    hour: "16",
+                    minutes: "25"
                 }, {
-                    hour: 17,
-                    minutes: 15
+                    hour: "17",
+                    minutes: "15"
                 }, {
-                    hour: 17,
-                    minutes: 30
+                    hour: "17",
+                    minutes: "30"
                 }, {
-                    hour: 18,
-                    minutes: 5
+                    hour: "18",
+                    minutes: "05"
                 }, {
-                    hour: 18,
-                    minutes: 40
+                    hour: "18",
+                    minutes: "40"
                 }, {
-                    hour: 19,
-                    minutes: 15
+                    hour: "19",
+                    minutes: "15"
                 }, {
-                    hour: 19,
-                    minutes: 50
+                    hour: "19",
+                    minutes: "50"
                 },
             ]
         };
