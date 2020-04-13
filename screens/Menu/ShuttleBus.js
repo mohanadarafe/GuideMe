ce64b7dde628bf044e193a9600c092f0dcf776f5
Kenza <kenza.boulisfane@gmail.com>
2020-04-13 19:29:53 -0400

    Fix: allow double digits when returning the time - cleanup code - add js doc comments [#167]

--- screens/Menu/ShuttleBus.js
+++ screens/Menu/ShuttleBus.js
@@ -5,9 +5,11 @@ import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
 import SegmentedControlTab from "react-native-segmented-control-tab";
 import { ShuttleBusTimes } from "../../constants/shuttleBustimes";
-import { Button } from "react-native-paper";
-import { Icon } from "native-base";
 
+/**
+ * This a function is a mini component that returns the layout and the data of each active tab
+ * @param {*} props 
+ */
 function TimesToDisplay (props) {
     let DATA = [];
     let campus = props.campus;
@@ -17,7 +19,6 @@ function TimesToDisplay (props) {
     else if (props.campus == "Loyola") {
         DATA = props.data.LoyolaStops;
     }
-    // console.log("data: ", props.data);
     return (
         <SafeAreaView style={{ top: "2%", width: "100%" }}>
             <FlatList
@@ -27,14 +28,20 @@ function TimesToDisplay (props) {
                 renderItem={({ item }) => (
                     <ListItem
                         title={`Shuttle Bus to ${campus}`}
-                        titleStyle={{ color: "white", marginLeft: "5%" }}
+                        titleStyle={{
+                            color: "white", marginLeft: "5%",
+                        }}
                         subtitle={`${item.hour}:${item.minutes}`}
-                        subtitleStyle={{ color: "white", marginLeft: "5%" }}
+                        subtitleStyle={{
+                            color: "white", marginLeft: "5%", fontFamily: "encodeSansExpanded",
+                        }}
                         bottomDivider
                         containerStyle={{ backgroundColor: "#2A2E43", borderRadius: 5, marginHorizontal: "2%" }}
-                        leftIcon={<MaterialCommunityIcons name="bus-clock" size={ICON_SIZE} style={styles.mapPin} />}
+                        leftIcon={<MaterialCommunityIcons name="bus-clock" size={ICON_SIZE} style={styles.leftIcon} />}
                         rightTitle={`${item.timeDifference.Hours}:${item.timeDifference.Minutes}`}
-                        rightTitleStyle={{ color: "#fff" }}
+                        rightTitleStyle={{
+                            color: "grey", fontFamily: "encodeSansExpanded", fontWeight: "bold"
+                        }}
                     />
                 )
                 }
@@ -44,15 +51,12 @@ function TimesToDisplay (props) {
 }
 
 TimesToDisplay.propTypes = {
-    navigation: PropTypes.object,
-    openDrawer: PropTypes.func,
+    props: PropTypes.object,
+    campus: PropTypes.any,
+    data: PropTypes.any
 };
 
-
-//TODO: create a hook called results that takes as a parameter getNextStops in the useEffect()
-
 const ICON_SIZE = 35;
-
 
 /** Prop passed
  * @param  {} navigation props.navigation is the name of the object from Navigator library
@@ -64,38 +68,10 @@ function ShuttleBus (props) {
     const [campus, setCampus] = React.useState("SGW");
     const [results, setResults] = React.useState([]);
 
-
     const weekDays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "saturday", "sunday"];
     const friday = ["friday"];
 
     const getShuttleBusTimes = ShuttleBusTimes();
-    const sgw = "SGW";
-    const loyola = "Loyola";
-
-    const data = {
-        "users":
-            [
-                {
-                    "name": "Proxima Midnight",
-                    "email": "proxima@appdividend.com",
-                    "hour": "5",
-                    "minutes": "20"
-                },
-                {
-                    "name": "Ebony Maw",
-                    "email": "ebony@appdividend.com",
-                    "hour": "6",
-                    "minutes": "20"
-                },
-                {
-                    "name": "Black Dwarf",
-                    "email": "dwarf@appdividend.com",
-                    "hour": "7",
-                    "minutes": "20"
-
-                },
-            ]
-    };
 
     /**
     * The method will slide the side menu from the right side of the screen
@@ -113,21 +89,16 @@ function ShuttleBus (props) {
         let currentMinutes = new Date().getMinutes();
         //set current time 
         setCurrentTime({ Hour: currentHour, Minutes: currentMinutes });
+
         let CurrentDayIndex = new Date().getDay();
         let days = [];
         days = weekDays;
         days.forEach((item, index) => {
             if (index == CurrentDayIndex) {
+                //set current day
                 setCurrentDay(item);
             }
         });
-        // days.forEach((item, key) => {
-        //     if (key == CurrentDayIndex) {
-        //         //set current day
-        //         console.log("INSIDE");
-        //         setCurrentDay(item);
-        //     }
-        // });
     };
 
     /**
@@ -138,10 +109,19 @@ function ShuttleBus (props) {
     const calculateTimeDifference = (nextStop) => {
         var diffHours = nextStop.hour - currentTime.Hour;
         var diffMinutes = nextStop.minutes - currentTime.Minutes;
-        return ({
-            Hours: diffHours,
-            Minutes: Math.abs(diffMinutes)
-        });
+        if (Math.abs(diffMinutes) < 10) {
+            return ({
+                Hours: diffHours,
+                Minutes: "0" + Math.abs(diffMinutes)
+            });
+        }
+        else {
+            return ({
+                Hours: diffHours,
+                Minutes: Math.abs(diffMinutes)
+            });
+        }
+
     };
 
     const fetchNextStops = (list) => {
@@ -150,7 +130,6 @@ function ShuttleBus (props) {
         let index = 0;
         for (var key in list) {
             if (list[key].hour >= currentTime.Hour && list[key].minutes >= currentTime.Minutes) {
-                console.log("Never here");
                 nextStops = list.slice(index, list.length - 1);
                 break;
             }
@@ -159,7 +138,7 @@ function ShuttleBus (props) {
         if (nextStops.length > 0) {
             results = nextStops.map((element, index) => {
                 return ({
-                    id: index,
+                    id: index.toString(),
                     timeDifference: calculateTimeDifference(element),
                     hour: element.hour,
                     minutes: element.minutes
@@ -177,7 +156,6 @@ function ShuttleBus (props) {
         var scheduleTimesLoyola = [];
         const sgwCampus = "SGW";
         const loyolaCampus = "Loyola";
-        var nextStops = [];
         let index = 0;
         var results = [];
         if (currentDay && weekDays.includes(currentDay.toString())) {
@@ -193,7 +171,6 @@ function ShuttleBus (props) {
         }
         var nextStopsSGW = fetchNextStops(scheduleTimesSGW);
         var nextStopsLoyola = fetchNextStops(scheduleTimesLoyola);
-        // console.log(nextStopsSGW, nextStopsLoyola);
         return ({
             sgwStops: nextStopsSGW,
             LoyolaStops: nextStopsLoyola
@@ -209,7 +186,6 @@ function ShuttleBus (props) {
         }
         return () => clearInterval(intervalId);
     }, [currentTime, currentDay, campus]);
-
 
     return (
         <View style={styles.container}>
@@ -255,11 +231,9 @@ function ShuttleBus (props) {
                     }}
                 />
                 {selectedTab === 0 && (
-                    // <Text style={styles.tabContent}>HEYYY 1</Text>
                     <TimesToDisplay campus="SGW" data={results} />
                 )}
                 {selectedTab === 1 && (
-                    // <Text style={styles.tabContent}>HEYY 2</Text>
                     <TimesToDisplay campus="Loyola" data={results} />
                 )}
             </View>
@@ -276,30 +250,22 @@ ShuttleBus.propTypes = {
 export const styles = StyleSheet.create({
     container: {
         alignItems: "center",
-        // justifyContent: "space-between",
         height: "100%",
         width: "100%",
         flexDirection: "column",
         backgroundColor: "#2A2E43" // go back to this when done
-        // backgroundColor: "red"
     },
     mainLabel: {
-        // position: "absolute",
         color: "#FFFFFF",
         fontSize: 30,
         fontWeight: "bold",
         fontFamily: "encodeSansExpanded",
-        // top: "25%",
-        // right: "55%"
     },
     shortLabel: {
-        // position: "absolute",
         color: "#FFFFFF",
         opacity: 0.7,
         fontSize: 15,
         fontFamily: "encodeSansExpanded",
-        // top: "29%",
-        // right: "52%"
     },
     icon: {
         alignSelf: "center",
@@ -329,14 +295,7 @@ export const styles = StyleSheet.create({
         top: "0%",
         position: "relative"
     },
-    controlTabContainer: {
-        top: "72%",
-        alignContent: "space-between",
-        position: "absolute" // not sure about adding this
-    },
     tabsContainerStyle: {
-        // bottom: "100%",
-        // top: "-130%",
         backgroundColor: "#2A2E43",
     },
     tabStyle: {
@@ -359,127 +318,10 @@ export const styles = StyleSheet.create({
         backgroundColor: "#2A2E43",
         fontWeight: "bold"
     },
-    test: {
-        backgroundColor: "yellow",
-        bottom: "100%",
-        top: "-130%",
-    },
-    tabContainer: {
-        // flex: 1,
-        position: "absolute",
-        top: "0.05%",
-        // bottom: "1%",
-        backgroundColor: "green",
-        width: "200%",
-        height: "1500%",
-        alignSelf: "center",
-        paddingHorizontal: "5%",
-    },
-    tabContent: {
-        color: "#fff",
-        fontSize: 18,
-        margin: 24,
-        // bottom: "100%",
-        width: "100%",
-    },
-    mapPin: {
+    leftIcon: {
         color: "#FFFFFF",
         position: "absolute",
-        // marginLeft: "5%",
-        // marginRight: "5%"
-
     },
-    itemContainer: {
-        flexDirection: "column",
-        justifyContent: "flex-end",
-        height: "70%",
-        width: "100%",
-    },
-    iconContainer: {
-        height: "100%",
-        width: ICON_SIZE * 2,
-        backgroundColor: "#353A50",
-        borderRadius: 10,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    itemTextContainer: {
-        height: "100%",
-        width: "100%",
-        // justifyContent: "center",
-        alignSelf: "flex-end",
-    },
-    buttonContainer: {
-        top: "40%",
-        flexDirection: "column",
-        justifyContent: "flex-end",
-        height: "100%",
-        width: "90%",
-        alignItems: "center",
-    },
-    separator: {
-        height: "100%",
-        width: "4%",
-        justifyContent: "center",
-    },
-    mapPinLabel: {
-        color: "#FFFFFF",
-        fontSize: 13,
-        fontFamily: "encodeSansExpanded",
-        position: "absolute",
-    },
-    smallLabel: {
-        color: "#FFFFFF",
-        fontSize: 10,
-        fontFamily: "encodeSansExpanded",
-        bottom: "10%"
-        // position: "absolute",
-    },
-    buttonTextContainer: {
-        height: "100%",
-        width: "80%",
-        justifyContent: "center",
-        alignSelf: "flex-end",
-    },
-    flatview: {
-        top: "200%",
-        // justifyContent: "center", //original
-        justifyContent: "space-between",
-        paddingTop: 30,
-        borderRadius: 2,
-        backgroundColor: "orange"
-    },
-    name: {
-        fontFamily: "Verdana",
-        fontSize: 10,
-        color: "white"
-    },
-    email: {
-        color: "green"
-    },
-    phone: {
-        position: "absolute",
-        color: "#FFFFFF",
-        alignSelf: "center"
-    },
-    phoneLabel: {
-        position: "absolute",
-        color: "#FFFFFF",
-        fontSize: 13,
-        fontFamily: "encodeSansExpanded"
-    },
-    buttonTextContainer: {
-        height: "100%",
-        width: "80%",
-        justifyContent: "center",
-        alignSelf: "flex-end"
-    },
-    separator: {
-        height: "100%",
-        width: "4%",
-        justifyContent: "center",
-    },
-
 });
 
 export default ShuttleBus;
