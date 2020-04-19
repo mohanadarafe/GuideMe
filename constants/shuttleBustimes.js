a96991a741536f887032a7003f058616771625e7
Kenza <kenza.boulisfane@gmail.com>
2020-04-18 20:14:08 -0400

    potential fix to code duplication [#239]

--- constants/shuttleBustimes.js
+++ constants/shuttleBustimes.js
@@ -1,495 +1,3 @@
-const Loyola_Weekdays = [
-    {
-        hour: "7",
-        minutes: "30"
-    },
-    {
-        hour: "7",
-        minutes: "40"
-    },
-    {
-        hour: "7",
-        minutes: "55"
-    },
-    {
-        hour: "8",
-        minutes: "20"
-    },
-    {
-        hour: "8",
-        minutes: "35"
-    },
-    {
-        hour: "8",
-        minutes: "55"
-    },
-    {
-        hour: "9",
-        minutes: "10"
-    },
-    {
-        hour: "9",
-        minutes: "30"
-    },
-    {
-        hour: "9",
-        minutes: "45"
-    },
-    {
-        hour: "10",
-        minutes: "20"
-    },
-    {
-        hour: "10",
-        minutes: "35"
-    },
-    {
-        hour: "10",
-        minutes: "55"
-    },
-    {
-        hour: "11",
-        minutes: "10"
-    },
-    {
-        hour: "11",
-        minutes: "30"
-    },
-    {
-        hour: "12",
-        minutes: "00"
-    },
-    {
-        hour: "12",
-        minutes: "30"
-    },
-    {
-        hour: "13",
-        minutes: "00"
-    },
-    {
-        hour: "13",
-        minutes: "30"
-    },
-    {
-        hour: "14",
-        minutes: "00"
-    },
-    {
-        hour: "14",
-        minutes: "30"
-    },
-    {
-        hour: "15",
-        minutes: "00"
-    },
-    {
-        hour: "15",
-        minutes: "30"
-    },
-    {
-        hour: "16",
-        minutes: "00"
-    },
-    {
-        hour: "16",
-        minutes: "30"
-    },
-    {
-        hour: "17",
-        minutes: "00"
-    },
-    {
-        hour: "17",
-        minutes: "30"
-    },
-    {
-        hour: "18",
-        minutes: "00"
-    },
-    {
-        hour: "18",
-        minutes: "30"
-    },
-    {
-        hour: "19",
-        minutes: "00"
-    },
-    {
-        hour: "19",
-        minutes: "30"
-    },
-    {
-        hour: "20",
-        minutes: "00"
-    },
-    {
-        hour: "20",
-        minutes: "30"
-    },
-    {
-        hour: "20",
-        minutes: "50"
-    },
-    {
-        hour: "21",
-        minutes: "05"
-    },
-    {
-        hour: "21",
-        minutes: "30"
-    },
-    {
-        hour: "22",
-        minutes: "00"
-    },
-    {
-        hour: "22",
-        minutes: "30"
-    },
-    {
-        hour: "23",
-        minutes: "00"
-    }
-];
-
-const Loyola_Friday = [
-    {
-        hour: "7",
-        minutes: "40"
-    },
-    {
-        hour: "8",
-        minutes: "15"
-    },
-    {
-        hour: "8",
-        minutes: "55"
-    },
-    {
-        hour: "9",
-        minutes: "30"
-    },
-    {
-        hour: "10",
-        minutes: "20"
-    },
-    {
-        hour: "10",
-        minutes: "35"
-    },
-    {
-        hour: "11",
-        minutes: "10"
-    },
-    {
-        hour: "11",
-        minutes: "45"
-    },
-    {
-        hour: "12",
-        minutes: "20"
-    },
-    {
-        hour: "12",
-        minutes: "55"
-    },
-    {
-        hour: "13",
-        minutes: "30"
-    },
-    {
-        hour: "14",
-        minutes: "05"
-    },
-    {
-        hour: "14",
-        minutes: "40"
-    },
-    {
-        hour: "15",
-        minutes: "15"
-    },
-    {
-        hour: "15",
-        minutes: "50"
-    },
-    {
-        hour: "16",
-        minutes: "25"
-    },
-    {
-        hour: "16",
-        minutes: "40"
-    },
-    {
-        hour: "17",
-        minutes: "00"
-    },
-    {
-        hour: "18",
-        minutes: "05"
-    },
-    {
-        hour: "18",
-        minutes: "40"
-    },
-    {
-        hour: "19",
-        minutes: "15"
-    },
-    {
-        hour: "19",
-        minutes: "50"
-    }
-];
-
-const SGW_Weekdays = [
-    {
-        hour: "7",
-        minutes: "45"
-    },
-    {
-        hour: "8",
-        minutes: "05"
-    },
-    {
-        hour: "8",
-        minutes: "20"
-    },
-    {
-        hour: "8",
-        minutes: "35"
-    },
-    {
-        hour: "8",
-        minutes: "55"
-    },
-    {
-        hour: "9",
-        minutes: "10"
-    },
-    {
-        hour: "9",
-        minutes: "30"
-    },
-    {
-        hour: "9",
-        minutes: "45"
-    },
-    {
-        hour: "10",
-        minutes: "05"
-    },
-    {
-        hour: "10",
-        minutes: "20"
-    },
-    {
-        hour: "10",
-        minutes: "55"
-    },
-    {
-        hour: "11",
-        minutes: "10"
-    },
-    {
-        hour: "11",
-        minutes: "45"
-    },
-    {
-        hour: "12",
-        minutes: "15"
-    },
-    {
-        hour: "12",
-        minutes: "45"
-    },
-    {
-        hour: "13",
-        minutes: "15"
-    },
-    {
-        hour: "13",
-        minutes: "45"
-    },
-    {
-        hour: "14",
-        minutes: "15"
-    },
-    {
-        hour: "14",
-        minutes: "45"
-    },
-    {
-        hour: "15",
-        minutes: "15"
-    },
-    {
-        hour: "15",
-        minutes: "45"
-    },
-    {
-        hour: "16",
-        minutes: "15"
-    },
-    {
-        hour: "16",
-        minutes: "45"
-    },
-    {
-        hour: "17",
-        minutes: "15"
-    },
-    {
-        hour: "17",
-        minutes: "45"
-    },
-    {
-        hour: "18",
-        minutes: "15"
-    },
-    {
-        hour: "18",
-        minutes: "45"
-    },
-    {
-        hour: "19",
-        minutes: "15"
-    },
-    {
-        hour: "19",
-        minutes: "45"
-    },
-    {
-        hour: "20",
-        minutes: "00"
-    },
-    {
-        hour: "20",
-        minutes: "15"
-    },
-    {
-        hour: "20",
-        minutes: "30"
-    },
-    {
-        hour: "21",
-        minutes: "00"
-    },
-    {
-        hour: "21",
-        minutes: "25"
-    },
-    {
-        hour: "21",
-        minutes: "45"
-    },
-    {
-        hour: "22",
-        minutes: "05"
-    },
-    {
-        hour: "22",
-        minutes: "30"
-    },
-    {
-        hour: "23",
-        minutes: "00"
-    }
-];
-
-const SGW_Friday = [
-    {
-        hour: "7",
-        minutes: "45"
-    },
-    {
-        hour: "8",
-        minutes: "20"
-    },
-    {
-        hour: "8",
-        minutes: "55"
-    },
-    {
-        hour: "9",
-        minutes: "30"
-    },
-    {
-        hour: "10",
-        minutes: "05"
-    },
-    {
-        hour: "10",
-        minutes: "55"
-    },
-    {
-        hour: "11",
-        minutes: "10"
-    },
-    {
-        hour: "11",
-        minutes: "45"
-    },
-    {
-        hour: "12",
-        minutes: "20"
-    },
-    {
-        hour: "12",
-        minutes: "55"
-    },
-    {
-        hour: "13",
-        minutes: "30"
-    },
-    {
-        hour: "14",
-        minutes: "05"
-    },
-    {
-        hour: "14",
-        minutes: "40"
-    },
-    {
-        hour: "15",
-        minutes: "15"
-    },
-    {
-        hour: "15",
-        minutes: "50"
-    },
-    {
-        hour: "16",
-        minutes: "25"
-    },
-    {
-        hour: "17",
-        minutes: "15"
-    },
-    {
-        hour: "17",
-        minutes: "30"
-    },
-    {
-        hour: "18",
-        minutes: "05"
-    },
-    {
-        hour: "18",
-        minutes: "40"
-    },
-    {
-        hour: "19",
-        minutes: "15"
-    },
-    {
-        hour: "19",
-        minutes: "50"
-    }
-];
-
 export const ShuttleBusTimes = () => {
     const schedule = {
         "Loyola": {
@@ -503,6 +11,382 @@ export const ShuttleBusTimes = () => {
     };
     return schedule;
 };
+
+const Loyola_Weekdays = [
+    {
+        hour: "7",
+        minutes: "30"
+    }, {
+        hour: "7",
+        minutes: "40"
+    }, {
+        hour: "7",
+        minutes: "55"
+    }, {
+        hour: "8",
+        minutes: "20"
+    }, {
+        hour: "8",
+        minutes: "35"
+    }, {
+        hour: "8",
+        minutes: "55"
+    }, {
+        hour: "9",
+        minutes: "10"
+    }, {
+        hour: "9",
+        minutes: "30"
+    }, {
+        hour: "9",
+        minutes: "45"
+    }, {
+        hour: "10",
+        minutes: "20"
+    }, {
+        hour: "10",
+        minutes: "35"
+    }, {
+        hour: "10",
+        minutes: "55"
+    }, {
+        hour: "11",
+        minutes: "10"
+    }, {
+        hour: "11",
+        minutes: "30"
+    }, {
+        hour: "12",
+        minutes: "00"
+    }, {
+        hour: "12",
+        minutes: "30"
+    }, {
+        hour: "13",
+        minutes: "00"
+    }, {
+        hour: "13",
+        minutes: "30"
+    }, {
+        hour: "14",
+        minutes: "00"
+    }, {
+        hour: "14",
+        minutes: "30"
+    }, {
+        hour: "15",
+        minutes: "00"
+    }, {
+        hour: "15",
+        minutes: "30"
+    }, {
+        hour: "16",
+        minutes: "00"
+    }, {
+        hour: "16",
+        minutes: "30"
+    }, {
+        hour: "17",
+        minutes: "00"
+    }, {
+        hour: "17",
+        minutes: "30"
+    }, {
+        hour: "18",
+        minutes: "00"
+    }, {
+        hour: "18",
+        minutes: "30"
+    }, {
+        hour: "19",
+        minutes: "00"
+    }, {
+        hour: "19",
+        minutes: "30"
+    }, {
+        hour: "20",
+        minutes: "00"
+    }, {
+        hour: "20",
+        minutes: "30"
+    }, {
+        hour: "20",
+        minutes: "50"
+    }, {
+        hour: "21",
+        minutes: "05"
+    }, {
+        hour: "21",
+        minutes: "30"
+    }, {
+        hour: "22",
+        minutes: "00"
+    }, {
+        hour: "22",
+        minutes: "30"
+    }, {
+        hour: "23",
+        minutes: "00"
+    }
+];
+
+const Loyola_Friday = [
+    {
+        hour: "7",
+        minutes: "40"
+    }, {
+        hour: "8",
+        minutes: "15"
+    }, {
+        hour: "8",
+        minutes: "55"
+    }, {
+        hour: "9",
+        minutes: "30"
+    }, {
+        hour: "10",
+        minutes: "20"
+    }, {
+        hour: "10",
+        minutes: "35"
+    }, {
+        hour: "11",
+        minutes: "10"
+    }, {
+        hour: "11",
+        minutes: "45"
+    }, {
+        hour: "12",
+        minutes: "20"
+    }, {
+        hour: "12",
+        minutes: "55"
+    }, {
+        hour: "13",
+        minutes: "30"
+    }, {
+        hour: "14",
+        minutes: "05"
+    }, {
+        hour: "14",
+        minutes: "40"
+    }, {
+        hour: "15",
+        minutes: "15"
+    }, {
+        hour: "15",
+        minutes: "50"
+    }, {
+        hour: "16",
+        minutes: "25"
+    }, {
+        hour: "16",
+        minutes: "40"
+    }, {
+        hour: "17",
+        minutes: "00"
+    }, {
+        hour: "18",
+        minutes: "05"
+    }, {
+        hour: "18",
+        minutes: "40"
+    }, {
+        hour: "19",
+        minutes: "15"
+    }, {
+        hour: "19",
+        minutes: "50"
+    }
+];
+
+const SGW_Weekdays = [
+    {
+        hour: "7",
+        minutes: "45"
+    }, {
+        hour: "8",
+        minutes: "05"
+    }, {
+        hour: "8",
+        minutes: "20"
+    }, {
+        hour: "8",
+        minutes: "35"
+    }, {
+        hour: "8",
+        minutes: "55"
+    }, {
+        hour: "9",
+        minutes: "10"
+    }, {
+        hour: "9",
+        minutes: "30"
+    }, {
+        hour: "9",
+        minutes: "45"
+    }, {
+        hour: "10",
+        minutes: "05"
+    }, {
+        hour: "10",
+        minutes: "20"
+    }, {
+        hour: "10",
+        minutes: "55"
+    }, {
+        hour: "11",
+        minutes: "10"
+    }, {
+        hour: "11",
+        minutes: "45"
+    }, {
+        hour: "12",
+        minutes: "15"
+    }, {
+        hour: "12",
+        minutes: "45"
+    }, {
+        hour: "13",
+        minutes: "15"
+    }, {
+        hour: "13",
+        minutes: "45"
+    }, {
+        hour: "14",
+        minutes: "15"
+    }, {
+        hour: "14",
+        minutes: "45"
+    }, {
+        hour: "15",
+        minutes: "15"
+    }, {
+        hour: "15",
+        minutes: "45"
+    }, {
+        hour: "16",
+        minutes: "15"
+    }, {
+        hour: "16",
+        minutes: "45"
+    }, {
+        hour: "17",
+        minutes: "15"
+    }, {
+        hour: "17",
+        minutes: "45"
+    }, {
+        hour: "18",
+        minutes: "15"
+    }, {
+        hour: "18",
+        minutes: "45"
+    }, {
+        hour: "19",
+        minutes: "15"
+    }, {
+        hour: "19",
+        minutes: "45"
+    }, {
+        hour: "20",
+        minutes: "00"
+    }, {
+        hour: "20",
+        minutes: "15"
+    }, {
+        hour: "20",
+        minutes: "30"
+    }, {
+        hour: "21",
+        minutes: "00"
+    }, {
+        hour: "21",
+        minutes: "25"
+    }, {
+        hour: "21",
+        minutes: "45"
+    }, {
+        hour: "22",
+        minutes: "05"
+    }, {
+        hour: "22",
+        minutes: "30"
+    }, {
+        hour: "23",
+        minutes: "00"
+    }
+];
+
+const SGW_Friday = [
+    {
+        hour: "7",
+        minutes: "45"
+    }, {
+        hour: "8",
+        minutes: "20"
+    }, {
+        hour: "8",
+        minutes: "55"
+    }, {
+        hour: "9",
+        minutes: "30"
+    }, {
+        hour: "10",
+        minutes: "05"
+    }, {
+        hour: "10",
+        minutes: "55"
+    }, {
+        hour: "11",
+        minutes: "10"
+    }, {
+        hour: "11",
+        minutes: "45"
+    }, {
+        hour: "12",
+        minutes: "20"
+    }, {
+        hour: "12",
+        minutes: "55"
+    }, {
+        hour: "13",
+        minutes: "30"
+    }, {
+        hour: "14",
+        minutes: "05"
+    }, {
+        hour: "14",
+        minutes: "40"
+    }, {
+        hour: "15",
+        minutes: "15"
+    }, {
+        hour: "15",
+        minutes: "50"
+    }, {
+        hour: "16",
+        minutes: "25"
+    }, {
+        hour: "17",
+        minutes: "15"
+    }, {
+        hour: "17",
+        minutes: "30"
+    }, {
+        hour: "18",
+        minutes: "05"
+    }, {
+        hour: "18",
+        minutes: "40"
+    }, {
+        hour: "19",
+        minutes: "15"
+    }, {
+        hour: "19",
+        minutes: "50"
+    }
+];
 
 
 
