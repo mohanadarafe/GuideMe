5dc4154ea68ffc3a579a378c90ddea67d6bb55c0
Alain D. Jobs <33582344+AlainJobs@users.noreply.github.com>
2020-04-19 16:34:26 -0400

    fixed Travis fail in test #242

--- __tests__/unit/Screens/SideMenu/Settings.test.js
+++ __tests__/unit/Screens/SideMenu/Settings.test.js
@@ -1,10 +1,20 @@
 import React from "react";
-import {Settings} from "../../../../screens/Menu/Settings";
+import { Settings } from "../../../../screens/Menu/Settings";
 import renderer from "react-test-renderer";
 import { TouchableOpacity, Switch } from "react-native";
 import { shallow } from "enzyme";
 
 describe("Settings component", () => {
+
+    const setState = jest.fn();
+    const useStateSpy = jest.spyOn(React, "useState");
+    useStateSpy.mockImplementation((init) => [init, setState]);
+    const props = {
+      isFetching: false,
+      dispatch: jest.fn(),
+      setDarkMode: (value) => {return value},
+    }
+
     test("renders correctly", () => {
         const tree = renderer.create(<Settings />).toJSON();
         expect(tree).toMatchSnapshot();
@@ -14,8 +24,7 @@ describe("Settings component", () => {
         const navigation = { openDrawer: (param, defaultValue) => {
             return "NearbyInterest";
         }};
-
-        const wrapper = shallow(<Settings navigation={navigation}/>);
+        const wrapper = shallow(<Settings {...props} navigation={navigation}/>);
         wrapper.find(TouchableOpacity).props().onPress();
         wrapper.find(Switch).at(0).simulate('valueChange')
     });
