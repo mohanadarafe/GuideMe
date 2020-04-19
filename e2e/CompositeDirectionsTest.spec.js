dc8c8e809ec2f8de5bd48cbb5976e253db4414a2
JayChouteau <47158909+JayChouteau@users.noreply.github.com>
2020-04-19 14:10:50 -0400

    Sprint 3 scenarios (#217)
    Added information on scenarios, steps and user stories
    Added assertions
    1 scenario moved from OUtdoor to Composite

--- e2e/CompositeDirectionsTest.spec.js
+++ e2e/CompositeDirectionsTest.spec.js
@@ -6,7 +6,7 @@ describe("Composite Directions Feature", () => {
    * US-15 : As a user, I want to be able to view directions on the map
    * US-25 : AS a user, I would like to be able to search for a room
    * US-26 : As a user, I would like to be able to set a room as my start or destination
-   * US-33 : AS a user, I would like to be able to be given directions between classes from two different campuses
+   * US-37 : As a user, I would like to navigate through building floors
    */
 
   /** BEFORE Each Test
@@ -17,34 +17,50 @@ describe("Composite Directions Feature", () => {
     await reloadApp();
   });
 
-  /**Scenario: Getting Directions from 1 room to another room on different campuses (SGW to LoyoLa)
-   * US-15 : As a user, I want to be able to view directions on the map
-   * US-25 : AS a user, I would like to be able to search for a room
-   * US-26 : As a user, I would like to be able to set a room as my start or destination
-   * US-33 : AS a user, I would like to be able to be given directions between classes from two different campuses
-   * 1. Click on the search bar
-   * 2. Enter the number of a room from Loyola Campus: "VL103"
-   * 3. Select the corresponding item from the dropdown list
-   * 4. Click on GET DIRECTIONS button
-   * 5. Check that TO search bar is filled with "VL103"
-   * 6. Click on the FROM search bar
-   * 7. Enter the number of a room on the SGW Campus: "H820"
-   * 8. Select the corresponding item from the dropdown list
-   * 9. Click on VIEW ROUTE
-   * TODO: Checking the path for directions is shown (need to play)
-   */    
-  it.skip("Getting Directions from 1 room to another room on different campuses (Hall to LoyoLa)", async () => {
+  /**Scenario: Get directions and naviagte from a building (D building) to a classroom (H819)
+  * US-15 : As a user, I want to be able to view directions on the map.
+  * US-28 : As a user, I would like to show the shortest path possible between my start and destination point.
+  * US-37 : As a user, I would like to navigate through building floors
+  * 1. Click on the search bar
+  * 2. Enter the name of the classroom (H819)
+  * 3. Select the corresponding item from the dropdown list
+  * 4. Click on GET DIRECTIONS button
+  * 5. Click on the FROM search bar
+  * 6. Enter the number of a building from the SGW campus: "D Building"
+  * 7. Select the corresponding item from the dropdown list
+  * 8. Click on VIEW ROUTE
+  * 9. Check that the preview map view exists
+  * 10. Check if the start button exists
+  * 11. Check if the directions button exists
+  * 12. Click the directions button three times will checking
+  * 13. Once the destionation is reached the inside building button is clicked
+  * 14. The first floor is examined and scrolled through
+  * 14. The eigth floor is examined and scrolled through
+  */
+  it("Get directions and naviagte from a building (D building) to a classroom (H819)", async () => {
     await element(by.id("Map_searchBar")).tap();
-    await element(by.id("Map_searchBar")).typeText("VL103");
-    await element(by.id("Map_searchBar")).tapAtPoint({ x: 200, y: 75 });
+    await element(by.id("Map_searchBar")).typeText("H819");
+    await element(by.id("Map_searchBar")).tapAtPoint({ x: 200, y: 80 });
     await element(by.id("BottomMenu_getDirectionsButton")).tap();
-    await expect(element(by.id("DoubleSearch_ToSearchBar"))).toHaveText("VL103");
-    await element(by.id("DoubleSearch_FromSearchBar")).tap();
-    await element(by.id("DoubleSearch_FromSearchBar")).typeText("H820");
-    //TODO: Check the x and y coordinates of the corresponding item in the dropdown list given relative position to the FROM Search Bar
-    //Fix possible: using items at index
-    await element(by.id("DoubleSearch_FromSearchBar")).atIndex(0).tapAtPoint();
+    await element(by.id("DoubleSearch_FromSearchBarViewFrom")).tap();
+    await element(by.id("DoubleSearch_FromSearchBarViewFrom")).typeText("D Building");
+    await element(by.id("DoubleSearch_FromSearchBarViewFrom")).tapAtPoint({ x: 200, y: 80 });
     await element(by.id("DoubleSearch_enabledViewRouteButton")).tap();
-    //TODO: Checking the path for directions is shown (need to play)
+    await expect(element(by.id("PreviewDirections_MapView"))).toExist();
+    await expect(element(by.id("BottomMenu_PreferenceStartButton"))).toBeVisible();
+    await element(by.id("BottomMenu_PreferenceStartButton")).tap();
+    await expect(element(by.id("Directions_BottomRightButton"))).toBeVisible();
+    await element(by.id("Directions_BottomRightButton")).tap();
+    await expect(element(by.id("Directions_BottomRightButton"))).toBeVisible();
+    await element(by.id("Directions_BottomRightButton")).tap();
+    await expect(element(by.id("Directions_BottomRightButton"))).toBeVisible();
+    await element(by.id("Directions_BottomRightButton")).tap();
+    await expect(element(by.id("Directions_InsideBuildingButton"))).toBeVisible();
+    await element(by.id("Directions_InsideBuildingButton")).tap();
+    await expect(element(by.id("IndoorMapView_FloorScrollView"))).toBeVisible();
+    await element(by.id("IndoorMapView_FloorScrollView")).swipe("left", "slow", 0.5);
+    await element(by.id("FloorMenu_floorBarMenuView")).tapAtPoint({ x: 180, y: 23 });
+    await expect(element(by.id("IndoorMapView_FloorScrollView"))).toExist();
+    await element(by.id("IndoorMapView_FloorScrollView")).swipe("left", "slow");
   });
 });
--- e2e/OutdoorDirectionsTest.spec.js
+++ e2e/OutdoorDirectionsTest.spec.js
@@ -8,6 +8,7 @@ describe("Outdoor Directions Feature", () => {
   * US-18 : As a user, I should be able to choose "walking" as a means of transportation.
   * US-19 : As a user, I should be able to choose public transport as a means of transportation.
   * US-20 : As a user, I should be able to choose "driving" as a means of transportation.
+  * US-27 : As a user, I would like to navigate from my desired start point/current location to my desired destination
   * US-28 : As a user, I would like to show the shortest path possible between my start and destination point.
   * US-40 : As a user, I should be able to choose "biking" as a means of transportation.
   */
@@ -43,9 +44,11 @@ describe("Outdoor Directions Feature", () => {
   * 17. Enter the name of a different building: "EV building"
   * 18. Select the corresponding item from the dropdown list
   * 19. Click on VIEW ROUTE
-  * 20. Check that the TO location is the expected one
-  * 21. Check that the FROM location is the expected one
-  * 22. Check that the path is visible on preview directions
+  * 20. Check that the preview map view exists
+  * 21. Check that the navigation header is present
+  * 22. Check that the TO location is the expected one
+  * 23. Check that the FROM location is the expected one
+  * 24. Check that the path is visible on preview directions
   */
   it.skip("Getting the preview directions for 1 building to another (from different campuses)", async () => {
     await element(by.id("Map_searchBar")).tap();
@@ -57,30 +60,32 @@ describe("Outdoor Directions Feature", () => {
     await element(by.id("DoubleSearch_FromSearchBarViewFrom")).tapAtPoint({ x: 200, y: 80 });
     await element(by.id("DoubleSearch_enabledViewRouteButton")).tap();
     await expect(element(by.id("PreviewDirections_MapView"))).toExist();
-    await expect(element(by.id("PreviewDirections_NavigationHeaderView"))).toExist();
-    //await expect(element(by.id("PreviewDirections_ToLocationText"))).toHaveText("VL Building");
-    //await expect(element(by.id("PreviewDirections_FromLocationText"))).toHaveText("Hall Building");
-    //await expect(element(by.id("PreviewDirection_MapViewPolyline"))).toExist();
-    //await expect(element(by.id("PreviewDirection_MapViewDefaultPolyline"))).toExist();
+    await expect(element(by.id("PreviewDirections_NavigationHeaderView"))).toBeVisible();
+    await expect(element(by.id("PreviewDirections_ToLocationText"))).toHaveLabel("VL Building");
+    await expect(element(by.id("PreviewDirections_FromLocationText"))).toHaveLabel("Hall Building");
+    //TODO: Find a solution for asserting the path shown on the map
     await element(by.id("PreviewDirections_GoBackIcon")).tap();
-    //await expect(element(by.id("DoubleSearch_ScreenView"))).toExist();
+    await expect(element(by.id("DoubleSearch_ScreenView"))).toBeVisible();
     await element(by.id("DoubleSearch_FromSearchBarViewFrom")).tap();
     await element(by.id("DoubleSearch_FromSearchBarViewFrom")).tapBackspaceKey();
     await element(by.id("DoubleSearch_FromSearchBarViewFrom")).typeText("Ev Building");
     await element(by.id("DoubleSearch_FromSearchBarViewFrom")).tapAtPoint({ x: 200, y: 80 });
     await element(by.id("DoubleSearch_enabledViewRouteButton")).tap();
     await expect(element(by.id("PreviewDirections_MapView"))).toExist();
-    await expect(element(by.id("PreviewDirections_NavigationHeaderView"))).toExist();
-    // //await expect(element(by.id("PreviewDirections_ToLocationText"))).toHaveText("VL Building");
-    // //await expect(element(by.id("PreviewDirections_FromLocationText"))).toHaveText("EV Building");
-    //await expect(element(by.id("PreviewDirection_MapViewPolyline"))).toExist();
+    await expect(element(by.id("PreviewDirections_NavigationHeaderView"))).toBeVisible();
+    await expect(element(by.id("PreviewDirections_ToLocationText"))).toHaveLabel("VL Building");
+    await expect(element(by.id("PreviewDirections_FromLocationText"))).toHaveLabel("EV Building");
+    //TODO: Find a solution for asserting the path shown on the map
   });
 
-  /**Scenario: Choosing from different Navigation options 
+  /**Scenario: Get Directions and choose from different navigation options 
+  * US-15 : As a user, I want to be able to view directions on the map.
+  * US-16 : As a user, I would like to be able to be given directions between campuses
   * US-18 : As a user, I should be able to choose "walking" as a means of transportation.
   * US-19 : As a user, I should be able to choose public transport as a means of transportation.
   * US-20 : As a user, I should be able to choose "driving" as a means of transportation.
   * US-40 : As a user, I should be able to choose "biking" as a means of transportation.
+  * US-28 : As a user, I would like to show the shortest path possible between my start and destination point.
   * 1. Click on the search bar
   * 2. Enter the name of a building from the Loyola Campus "VL building"
   * 3. Select the corresponding item from the dropdown list
@@ -99,7 +104,7 @@ describe("Outdoor Directions Feature", () => {
   * 16. Choose "biking" as a means of transportation
   * 17. Check that the path is swtiched to biking
   */
-  it.skip("Getting the preview directions for 1 building to another (from different campuses)", async () => {
+  it.skip("Get directions and choose from different navigation options", async () => {
     await element(by.id("Map_searchBar")).tap();
     await element(by.id("Map_searchBar")).typeText("VL Building");
     await element(by.id("Map_searchBar")).tapAtPoint({ x: 200, y: 75 });
@@ -109,34 +114,32 @@ describe("Outdoor Directions Feature", () => {
     await element(by.id("DoubleSearch_FromSearchBarViewFrom")).tapAtPoint({ x: 200, y: 80 });
     await element(by.id("DoubleSearch_enabledViewRouteButton")).tap();
     await expect(element(by.id("PreviewDirections_MapView"))).toExist();
-    //await expect(element(by.id("PreviewDirection_MapViewPolyline"))).toExist();
-    //await expect(element(by.id("PreviewDirection_MapViewDefaultPolyline"))).toExist();
+    //TODO: Find a solution for asserting the path shown on the map
     await element(by.id("BottomMenu_arrowUpToPreferenceMenuIcon")).tapAtPoint({ x: 10, y: 20 });
-    await expect(element(by.id("PreferenceMenu_ScreenView"))).toExist();
+    await expect(element(by.id("PreferenceMenu_ScreenView"))).toBeVisible();
     //TODO: 11. Check that "driving" is the means of transportation selected
     await element(by.id("PreferenceMenu_WalkingButton")).tap();
     await element(by.id("PreferenceMenu_GoBackIcon")).swipe('down', "slow");
     await expect(element(by.id("PreviewDirections_MapView"))).toExist();
-    //await expect(element(by.id("PreviewDirection_MapViewPolyline"))).toExist();
+    //TODO: Find a solution for asserting the path shown on the map
     await element(by.id("BottomMenu_arrowUpToPreferenceMenuIcon")).tapAtPoint({ x: 10, y: 20 });
-
-    await expect(element(by.id("PreferenceMenu_TransitButton"))).toExist();
+    await expect(element(by.id("PreferenceMenu_TransitButton"))).toBeVisible();
     await element(by.id("PreferenceMenu_TransitButton")).tap();
     await element(by.id("PreferenceMenu_GoBackIcon")).swipe('down', "slow");
     await expect(element(by.id("PreviewDirections_MapView"))).toExist();
-    //await expect(element(by.id("PreviewDirection_MapViewPolyline"))).toExist();
+    //TODO: Find a solution for asserting the path shown on the map
     await element(by.id("BottomMenu_arrowUpToPreferenceMenuIcon")).tapAtPoint({ x: 10, y: 20 });
-    await expect(element(by.id("PreferenceMenu_BicyclingButton"))).toExist();
+    await expect(element(by.id("PreferenceMenu_BicyclingButton"))).toBeVisible();
     await element(by.id("PreferenceMenu_BicyclingButton")).tap();
     await element(by.id("PreferenceMenu_GoBackIcon")).swipe('down', "slow");
     await expect(element(by.id("PreviewDirections_MapView"))).toExist();
-    //await expect(element(by.id("PreviewDirection_MapViewPolyline"))).toExist();
-
+    //TODO: Find a solution for asserting the path shown on the map
   });
 
-
-  /**Scenario: Exploring route direactions
-  * US-
+  /**Scenario: Get Directions and navigate from a desired start point to a desired destination
+  * US-15 : As a user, I want to be able to view directions on the map.
+  * US-27 : As a user, I would like to navigate from my desired start point/current location to my desired destination
+  * US-28 : As a user, I would like to show the shortest path possible between my start and destination point.
   * 1. Click on the search bar
   * 2. Enter the name of a building from the Loyola Campus "EN building"
   * 3. Select the corresponding item from the dropdown list
@@ -148,9 +151,9 @@ describe("Outdoor Directions Feature", () => {
   * 9. Check that the preview map view exists
   * 10. Check if the start button exists
   * 11. Check if the directions button exists
-  * 12. Click the directions button three times will checking
+  * 12. Click the directions button three times while checking
   */
-  it.skip("Get directions between buildings and route directions", async () => {
+  it.skip("Get directions and navigate from a desired start point to a desired destination", async () => {
     await element(by.id("Map_searchBar")).tap();
     await element(by.id("Map_searchBar")).typeText("EN Building");
     await element(by.id("Map_searchBar")).tapAtPoint({ x: 200, y: 80 });
@@ -160,71 +163,14 @@ describe("Outdoor Directions Feature", () => {
     await element(by.id("DoubleSearch_FromSearchBarViewFrom")).tapAtPoint({ x: 200, y: 80 });
     await element(by.id("DoubleSearch_enabledViewRouteButton")).tap();
     await expect(element(by.id("PreviewDirections_MapView"))).toExist();
-    await expect(element(by.id("BottomMenu_PreferenceStartButton"))).toExist();
-    await element(by.id("BottomMenu_PreferenceStartButton")).tap();
-
-    await expect(element(by.id("Directions_BottomRightButton"))).toExist();
+    await expect(element(by.id("BottomMenu_PreferenceStartButton"))).toBeVisible();
+    await element(by.id("BottomMenu_PreferenceStartButton")).tap(); 
+    await expect(element(by.id("Directions_BottomRightButton"))).toBeVisible();
     await element(by.id("Directions_BottomRightButton")).tap();
-
-    await expect(element(by.id("Directions_BottomRightButton"))).toExist();
+    await expect(element(by.id("Directions_BottomRightButton"))).toBeVisible();
     await element(by.id("Directions_BottomRightButton")).tap();
-
-    await expect(element(by.id("Directions_BottomRightButton"))).toExist();
+    await expect(element(by.id("Directions_BottomRightButton"))).toBeVisible();
     await element(by.id("Directions_BottomRightButton")).tap();
-
+    //TODO: Find a solution for asserting the path shown on the map
   });
-
-  /**Scenario: Get directions from a classroom to a building
-    * US-
-    * 1. Click on the search bar
-    * 2. Enter the name of the classroom (H819)
-    * 3. Select the corresponding item from the dropdown list
-    * 4. Click on GET DIRECTIONS button
-    * 5. Click on the FROM search bar
-    * 6. Enter the number of a building from the SGW campus: "D Building"
-    * 7. Select the corresponding item from the dropdown list
-    * 8. Click on VIEW ROUTE
-    * 9. Check that the preview map view exists
-    * 10. Check if the start button exists
-    * 11. Check if the directions button exists
-    * 12. Click the directions button three times will checking
-    * 13. Once the destionation is reached the inside building button is clicked
-    * 14. The first floor is examined and scrolled through
-    * 14. The eigth floor is examined and scrolled through
-    */
-  it("Get directions between building and classroom and route directions", async () => {
-    await element(by.id("Map_searchBar")).tap();
-    await element(by.id("Map_searchBar")).typeText("H819");
-    await element(by.id("Map_searchBar")).tapAtPoint({ x: 200, y: 80 });
-    await element(by.id("BottomMenu_getDirectionsButton")).tap();
-    await element(by.id("DoubleSearch_FromSearchBarViewFrom")).tap();
-    await element(by.id("DoubleSearch_FromSearchBarViewFrom")).typeText("D Building");
-    await element(by.id("DoubleSearch_FromSearchBarViewFrom")).tapAtPoint({ x: 200, y: 80 });
-    await element(by.id("DoubleSearch_enabledViewRouteButton")).tap();
-    await expect(element(by.id("PreviewDirections_MapView"))).toExist();
-    await expect(element(by.id("BottomMenu_PreferenceStartButton"))).toExist();
-    await element(by.id("BottomMenu_PreferenceStartButton")).tap();
-
-    await expect(element(by.id("Directions_BottomRightButton"))).toExist();
-    await element(by.id("Directions_BottomRightButton")).tap();
-
-    await expect(element(by.id("Directions_BottomRightButton"))).toExist();
-    await element(by.id("Directions_BottomRightButton")).tap();
-
-    await expect(element(by.id("Directions_BottomRightButton"))).toExist();
-    await element(by.id("Directions_BottomRightButton")).tap();
-
-    await expect(element(by.id("Directions_InsideBuildingButton"))).toExist();
-    await element(by.id("Directions_InsideBuildingButton")).tap();
-
-    await expect(element(by.id("IndoorMapView_FloorScrollView"))).toExist();
-    await element(by.id("IndoorMapView_FloorScrollView")).swipe("left", "slow", 0.5);
-
-    await element(by.id("FloorMenu_floorBarMenuView")).tapAtPoint({ x: 180, y: 23 });
-
-    await expect(element(by.id("IndoorMapView_FloorScrollView"))).toExist();
-    await element(by.id("IndoorMapView_FloorScrollView")).swipe("left", "slow");
-
-  });
-
 });
